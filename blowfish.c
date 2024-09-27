9874ddad551b6eda501553473f3963fe2a4d9c28
wearthkin <spicyghostpepe@gmail.com>
2024-09-26 21:45:37 -0500

    update

--- blowfish.c
+++ blowfish.c
@@ -19,7 +19,7 @@ unsigned char *in = NULL;
 void
 usage(void)
 {
-	fprintf(stderr, "USAGE: %s [-d] [-k file] [file]\n", argv0);
+	fprintf(stderr, "USAGE: %s [-d] [-k file] [-o output] [file]\n", argv0);
 	exit(0);
 }
 
@@ -30,9 +30,11 @@ int
 readw(int fd, uint32_t *x)
 {
 	int r;
+	uint32_t t;
 	r = read(fd, x, 8);
-	x[0] = ntohl(x[0]);
-	x[1] = ntohl(x[1]);
+	t = x[0];
+	x[0] = ntohl(x[1]);
+	x[1] = ntohl(t);
 	return r;
 }
 
@@ -40,19 +42,24 @@ void
 readws(unsigned char *buff, uint32_t *x)
 {
 	uint32_t *p = (uint32_t *) buff;
-	x[0] = ntohl(p[0]);
-	x[1] = ntohl(p[1]);
+	uint32_t t;
+	t = x[0];
+	x[0] = ntohl(p[1]);
+	x[1] = ntohl(t);
 }
 
 int
 writew(int fd, uint32_t *x, size_t len)
 {
+	uint32_t t;
 	int r;
-	x[0] = htonl(x[0]);
-	x[1] = htonl(x[1]);
+	t = x[0];
+	x[0] = htonl(x[1]);
+	x[1] = htonl(t);
 	r = write(fd, x, len);
-	x[0] = ntohl(x[0]);
-	x[1] = ntohl(x[1]);
+	t = x[0];
+	x[0] = ntohl(x[1]);
+	x[1] = ntohl(t);
 	return r;
 }
 
@@ -64,7 +71,8 @@ main(int argc, char **argv)
 	int fd = 0;
 	int ofd = 1;
 	unsigned char key[56];
-	char *a = NULL;
+	char *kfile = NULL;
+	char *ofile = NULL;
 	size_t keylen = 0;
 	char buff[BUFFSZ];
 	ssize_t rd;
@@ -80,24 +88,10 @@ main(int argc, char **argv)
 		decipher = 1;
 		break;
 	case 'k':
-		a = EARGF(usage());
-		if (!strcmp(a, "-"))
-			kfd = 0;
-		else {
-			kfd = open(a, O_RDONLY);
-			if (kfd < 0) {
-				perror(a);
-				return 1;
-			}
-		}
+		kfile = EARGF(usage());
 		break;
 	case 'o':
-		a = EARGF(usage());
-		ofd = open(a, O_WRONLY);
-		if (ofd < 0) {
-			fprintf(stderr, "Failed to open file for writing.");
-			return 1;
-		}
+		ofile = EARGF(usage());
 		break;
 	default:
 		usage();
@@ -109,11 +103,16 @@ main(int argc, char **argv)
 			perror(argv[0]);
 			return 1;
 		}
-	} else if (argc > 1 && kfd != -1)
+	} else if (argc > 1)
 		usage();
 
-
 	if (fd == 0) {
+		// Ensure the key is not also set as stdin
+		if (kfile != NULL && !strcmp(kfile, "-")) {
+			fprintf(stderr, "Key and input both stdin. Exiting.\n");
+			usage();
+		}
+
 		inlen = BUFFSZ;
 		in = malloc(inlen);
 		len = 0;
@@ -126,7 +125,7 @@ main(int argc, char **argv)
 
 		while ((rd = read(fd, buff, BUFFSZ)) > 0) {
 			if (rd < 0) {
-				fprintf(stderr, "Read error\n");
+				perror("read");
 				return 1;
 			}
 
@@ -147,25 +146,61 @@ main(int argc, char **argv)
 		inlen = lseek(fd, 0, SEEK_END);
 		lseek(fd, 0, SEEK_SET);
 		len = inlen;
+		// This will ensure zero padding if input length is not a
+		// multiple 8-bytes
 		inlen = ((inlen + 7) / 8) * 8;
 		in = malloc(inlen);
+
 		if (!in) {
 			fprintf(stderr, "Memory fail.\n");
 			return 1;
 		}
 
+		// zero-padding
 		memset(in, 0, inlen);
-		read(fd, in, inlen);
+		if (read(fd, in, inlen) < 0) {
+			perror("read");
+			free(in);
+			return 1;
+		}
 	}
 
-	if (kfd == fd)
-		usage();
+	if (kfile != NULL) {
+		if (!strcmp(kfile, "-"))
+			kfd = 0;
+		else
+			kfd = open(kfile, O_RDONLY);
 
-	if (kfd == -1) {
-		keylen=56;
-		memset(key, 0, sizeof key);
-	} else {
+		if (kfd < 0) {
+			perror("failed to open key file\n");
+			return 1;
+		}
+
+	}
+
+	if (kfd >= 0) {
 		keylen = read(kfd, key, sizeof key);
+
+		if (keylen < 0) {
+			perror("read");
+			free(in);
+			memset(key, 0, sizeof key);
+			return 1;
+		}
+
+		close(kfd);
+		kfd = -1;
+	}
+
+	if (ofile != NULL) {
+		ofd = open(ofile, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+		if (ofd < 0) {
+			perror("failed to open output file\n");
+			free(in);
+			memset(key, 0, sizeof key);
+			return 1;
+		}
 	}
 
 	bf_init(key, keylen);
@@ -173,35 +208,44 @@ main(int argc, char **argv)
 	keylen = 0;
 
 	if (decipher) {
+		// Start by reading the file length (first 8 bytes)
 		if (fd == 0) {
 			readws(in, (uint32_t *) &sz);
 			in += 8;
 		} else {
 			lseek(fd, 0, SEEK_SET);
 			readw(fd, (uint32_t *) &sz);
-			in = in + 8;
+			in += 8;
 		}
-
+		// This is accounts for the file length (first 8 bytes)
 		inlen -= 8;
 
 		for (uint32_t i = 0; i < inlen; i+= 8) {
 			memcpy(x, in + i, 8);
+			x[0] = ntohl(x[0]);
+			x[1] = ntohl(x[1]);
 			bf_decipher(x, x + 1);
+			// Ensure no more than `sz' bytes are written
 			if (i + 8 > sz)
-				write(ofd, x, sz - i);
+				rd = write(ofd, x, sz - i);
 			else
-				write(ofd, x, 8);
+				rd = write(ofd, x, 8);
 		}
 
 		ftruncate(ofd, sz);
 	} else {
+		// Start by writing out the file length
 		writew(ofd, (uint32_t *) &len, 8);
-		for (uint32_t i = 0; i < len; i+= 8) {
+		for (uint32_t i = 0; i < len; i += 8) {
 			memcpy(x, in + i, 8);
 			bf_encipher(x, x + 1);
+			x[0] = htonl(x[0]);
+			x[1] = htonl(x[1]);
 			write(ofd, x, 8);
 		}
 	}
+
+	close(ofd);
 
 	if (in) {
 		if (decipher) {
